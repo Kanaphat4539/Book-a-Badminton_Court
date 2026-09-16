347feaa104fe60652e4d4af2292a6c8b37c99d12
MIDTION <anwat251987@gmail.com>
2026-09-16 22:49:49 +0700

    feat(backend): implement shared features S01-S04

--- backend/src/cron/cron.module.ts
+++ backend/src/cron/cron.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CronService } from './cron.service';
 import { Booking } from '../bookings/entities/booking.entity';
+import { Student } from '../users/entities/student.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking])],
+  imports: [TypeOrmModule.forFeature([Booking, Student])],
   providers: [CronService]
 })
 export class CronModule {}
